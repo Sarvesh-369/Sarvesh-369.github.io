4722064319e1ce342938fbce8e400a80567cfb12
Sarvesh <sarvesh@Sarveshs-MacBook-Pro-1187.local>
2026-05-17 22:04:08 +0530

    fix navigation link resolution to use dynamic relative pathing

--- components/header.js
+++ components/header.js
@@ -4,40 +4,34 @@ class Header extends HTMLElement {
   }
 
   connectedCallback() {
-    const isGitHubPages = window.location.hostname.includes('github.io');
-    
-    // Dynamically calculate the root path (handles repository name on GitHub pages and '/' on localhost)
-    let rootPath = '/';
-    if (isGitHubPages) {
-      const pathSegments = window.location.pathname.split('/');
-      if (pathSegments.length > 1 && pathSegments[1]) {
-        rootPath = `/${pathSegments[1]}/`;
-      }
-    }
+    // Detect whether the current page is inside the 'pages' directory.
+    // This allows us to construct robust relative links regardless of where the site is hosted (localhost, user pages, or project pages).
+    const isPageSubdir = window.location.pathname.includes('/pages/');
+    const prefix = isPageSubdir ? '../' : './';
     
     this.innerHTML = `
       <header>
         <nav class="navbar navbar-expand-lg navbar-dark sticky-top">
-        <h1><a class="navbar-brand" href="${rootPath}index.html">Your Name</a></h1>
+        <h1><a class="navbar-brand" href="${prefix}index.html">Your Name</a></h1>
         <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="navbar-collapse collapse" id="navbarCollapse" style="">
           <ul class="navbar-nav ml-auto">
             <li class="nav-item mr-2">
-              <a class="nav-link" href="${rootPath}index.html">Home</a>
+              <a class="nav-link" href="${prefix}index.html">Home</a>
             </li>
             <li class="nav-item mr-2">
-              <a class="nav-link" href="${rootPath}pages/education.html">Education</a>
+              <a class="nav-link" href="${prefix}pages/education.html">Education</a>
             </li>
             <li class="nav-item mr-2">
-              <a class="nav-link" href="${rootPath}pages/experience.html">Experience</a>
+              <a class="nav-link" href="${prefix}pages/experience.html">Experience</a>
             </li>
             <li class="nav-item mr-2">
-              <a class="nav-link" href="${rootPath}pages/projects.html">Projects</a>
+              <a class="nav-link" href="${prefix}pages/projects.html">Projects</a>
             </li> 
             <li class="nav-item mr-2">
-              <a class="nav-link" href="${rootPath}pages/publications.html">Publications</a>
+              <a class="nav-link" href="${prefix}pages/publications.html">Publications</a>
             </li>
           </ul>
         </div>
